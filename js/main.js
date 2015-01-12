82f61a8449bf03d6eae0ff2a8acc2d984dd2cfa2
Andrei Constantinescu <andrei@constantinescu.io>
2015-01-12 18:29:30 +0200

    Animate progress bars when they enter in viewport.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-define(['jquery', './chart'], function ($) {
+define(['jquery', './chart', './viewportSelectors'], function ($) {
   window.App.book.on('render', function () {
-    $('.progress .progress-bar').progressbar();
+    var elements = $('.progress .progress-bar');
+
+    function onScroll(event) {
+      var inViewport = elements.filter(':in-viewport');
+      if (!inViewport.length) {
+        return;
+      }
+      elements = elements.not(inViewport);
+      inViewport.progressbar();
+      if (!elements.length) {
+        $('#scrollview').off('scroll', onScroll);
+      }
+    }
+
+    if (elements.length) {
+      $('#scrollview').bind("scroll", onScroll);
+      onScroll();
+    }
   });
 });
