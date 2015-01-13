b4696d5b548799678c723ea39eaaa48be05cc8f2
Andrei Constantinescu <andrei@constantinescu.io>
2015-01-13 17:04:46 +0200

    Add +bibliography mixin.

--- js/main.js
+++ js/main.js
@@ -18,6 +18,13 @@ define(['jquery', './chart', './viewportSelectors'], function ($) {
       $('#scrollview').bind("scroll", onScroll);
       onScroll();
     }
+
+    $('.textTooltip').each(function (idx, el) {
+      var $el = $(el);
+      var bibliography = window.bibliography[$el.attr("id")];
+      var content = '<div>'+ bibliography.author + ', <em>' + bibliography.title + '</em>(' + bibliography.location + ': ' + bibliography.publisher + (bibliography.year ? (', ' + bibliography.year) : '') + ') ' + (bibliography.pages ? (bibliography.pages + ', ') : '') + (bibliography.extra ? bibliography.extra : '') + '</div>';
+      $el.attr('data-content', content).html(bibliography.ref);
+    }).popover({html: true});
   });
 });
   WebFontConfig = {
@@ -32,3 +39,21 @@ define(['jquery', './chart', './viewportSelectors'], function ($) {
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
   })();
+
+  window.bibliography = {
+    livingstone2009: {
+      author: "Livingstone, S.",
+      ref: "Livingstone: 2009",
+      title: "Children and the Internet: Great expectations, challenging realities.",
+      year: "2009",
+      location: "Cambridge",
+      publisher: "Polity",
+    },
+    ito2009: {
+      author: "Ito, M et al.",
+      ref: "Ito: 2009",
+      title: "Hanging out, messing around, and geeking out: Kids living and learning with new media.",
+      location: "Cambridge, MA",
+      publisher: "MIT Press"
+    },
+  };
